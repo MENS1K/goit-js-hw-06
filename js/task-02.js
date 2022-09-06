3fb579addddbead9709d9abcb59725a04e64d95c
Misha Panivnyk <panivnykm@gmail.com>
2022-09-06 22:31:11 +0300

    add task1 and task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const listRef = document.querySelector("#ingredients");
+const listIngredientsRef = ingredients.map((ingredient) => {
+  const heading = document.createElement("li");
+  heading.classList = "item";
+  heading.textContent = ingredient;
+  return heading;
+});
+
+// console.log(heading);
+listRef.append(...listIngredientsRef);
