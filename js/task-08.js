c9d7ed93659927964fb2a605319b5d541e02cf7e
Misha Panivnyk <panivnykm@gmail.com>
2022-09-08 14:37:58 +0300

    add task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formRef = document.querySelector(".login-form");
 
+const formSubmit = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Заповніть всі поля!");
+  }
+  const userData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(userData);
+  event.currentTarget.reset();
+};
+formRef.addEventListener("submit", formSubmit);
