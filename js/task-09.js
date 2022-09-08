fb33aaedfe841cc8581f950cd64f4a07edc6a232
Misha Panivnyk <panivnykm@gmail.com>
2022-09-08 14:42:45 +0300

    add task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnRef = document.querySelector(".change-color");
+const colorRef = document.querySelector(".color");
+
+const btnClick = () => {
+  colorRef.textContent = document.body.style.background = getRandomHexColor();
+};
+btnRef.addEventListener("click", btnClick);
